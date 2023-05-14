a32b84d97e0fb9d5a282c52ff6d414e6080e6837
SamohtB <75974322+SamohtB@users.noreply.github.com>
2023-05-14 16:06:08 +0800

    Assignment 01 - Setup

---
+++ Assignment/01 - Setup/Setup/main.cpp
@@ -0,0 +1,46 @@
+#include <GLFW/glfw3.h>
+#include <cmath>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Thomas Banatin", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLE_FAN);
+        float angle = 0.0f;
+        for (int i = 0; i < 5; i++) {
+            glVertex3f(0.5 * cos(angle), 0.5 * sin(angle), 0.0f);
+            angle += 1.25664; //72 deg into radians
+        }
+        glEnd();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
