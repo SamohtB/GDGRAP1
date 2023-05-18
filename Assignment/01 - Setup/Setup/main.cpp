1969990f73ffc45984b1cbe7546ca927cbca0cbe
SamohtB <75974322+SamohtB@users.noreply.github.com>
2023-05-18 12:26:10 +0800

    Pink Bunny Movement

--- Assignment/01 - Setup/Setup/main.cpp
+++ Assignment/01 - Setup/Setup/main.cpp
@@ -5,6 +5,33 @@
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 
+#include <string>
+#include <iostream>
+
+float x_mod = 0;
+float y_mod = 0;
+
+void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mod)
+{
+    if (key == GLFW_KEY_D && action == GLFW_PRESS)
+    {
+        x_mod += 0.1f;
+    }
+    if (key == GLFW_KEY_A && action == GLFW_PRESS)
+    {
+        x_mod -= 0.1f;
+    }
+    if (key == GLFW_KEY_W && action == GLFW_PRESS)
+    {
+        y_mod += 0.1f;
+    }
+    if (key == GLFW_KEY_S && action == GLFW_PRESS)
+    {
+        y_mod -= 0.1f;
+    }
+
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -24,6 +51,35 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     gladLoadGL();
+
+    glfwSetKeyCallback(window, Key_Callback);
+
+    std::fstream vertSrc("Shaders/sample.vert");
+    std::stringstream vertBuff;
+    vertBuff << vertSrc.rdbuf();
+    std::string vertS = vertBuff.str();
+    const char* v = vertS.c_str();
+
+    std::fstream fragSrc("Shaders/sample.frag");
+    std::stringstream fragBuff;
+    fragBuff << fragSrc.rdbuf();
+    std::string fragS = fragBuff.str();
+    const char* f = fragS.c_str();
+
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertShader, 1, &v, NULL);
+    glCompileShader(vertShader);
+
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragShader, 1, &f, NULL);
+    glCompileShader(fragShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);   
+    
+
 
     std::string path = "3D/bunny.obj";
     std::vector<tinyobj::shape_t> shapes;
@@ -64,6 +120,9 @@ int main(void)
 
     //tells opengl that we're working on this VAO
     glBindVertexArray(VAO);
+
+    glUseProgram(shaderProgram);
+
     //Assign a VBO to the VAO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
@@ -94,11 +153,18 @@ int main(void)
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+    
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        unsigned int xLoc = glGetUniformLocation(shaderProgram, "x");
+        unsigned int yLoc = glGetUniformLocation(shaderProgram, "y");
+        glUniform1f(xLoc, x_mod);
+        glUniform1f(yLoc, y_mod);
 
         glBindVertexArray(VAO);
         glDrawElements(
