76dc638423f684ea26ffae0b8aaf9de90af9a7f6
SamohtB <75974322+SamohtB@users.noreply.github.com>
2023-06-10 22:30:55 +0800

    Complete Specs
    Complete Programming Challenge Specs.
    *Rotating seems weird -> double check expected output

--- main.cpp
+++ main.cpp
@@ -8,28 +8,79 @@
 #include <cmath>
 
 #include "Model3D.h"
+#include "main.h"
 
-float x_mod = 0.0f;
-float y_mod = 0.0f;
+//Camera LookAt and Camera Position
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 10.0f);
+glm::vec3 cameraCenter = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 worldUp = glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f));    //World's Up
 
-void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mod)
+bool firstMouse = true;
+float lastX = 300.0f;
+float lastY = 300.0f;
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+float previousTime = 0.0f;
+float deltaTime = 0.0f;
+
+void Key_Callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    if (key == GLFW_KEY_W && (action == GLFW_PRESS || action == GLFW_REPEAT))
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W) == GLFW_REPEAT)
     {
-        y_mod += 0.5f;
+        cameraPos.y += 300.f * deltaTime;
     }
-    if (key == GLFW_KEY_S && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_REPEAT)
     {
-        y_mod -= 0.5f;
+        cameraPos.y -= 300.f * deltaTime;
     }
-    if (key == GLFW_KEY_A && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_REPEAT)
     {
-        x_mod += 0.5f;
+        cameraPos.x -= 300.f * deltaTime;
     }
-    if (key == GLFW_KEY_D && (action == GLFW_PRESS || action == GLFW_REPEAT))
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_REPEAT)
     {
-        x_mod -= 0.5f;
+        cameraPos.x += 300.f * deltaTime;
     }
+}
+
+/* Mouse Function for rotation */
+void Mouse_Callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1) == GLFW_RELEASE) { firstMouse = true;  return; }
+    float xPos = (float)xpos;
+    float yPos = (float)ypos;
+    
+    if (firstMouse)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.f)
+        pitch = 89.f;
+    if (pitch < -89.f)
+        pitch = -89.f;
+    
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw) * cos(glm::radians(pitch)));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw) * cos(glm::radians(pitch)));
+    cameraCenter = glm::normalize(front);
 }
 
 /* Spawns Objects and Stores them in a vector named container
@@ -43,6 +94,36 @@ void SpawnObject(std::string sMeshPath, std::string sVertPath, std::string sFrag
      container->push_back(temp);
 }
 
+void SpawnModel(int state, std::vector<Model::Model3D*>& vecModel, glm::vec3& spawnPos)
+{
+    if (state == GLFW_PRESS)
+    {
+        //if timer reacher 3, allow spawn
+        if (glfwGetTime() >= 3.0f)
+        {
+            std::cout << "SPAWN!" << std::endl;
+
+            SpawnObject(
+                "3D/blasterA.obj",          //Path to 3D Model
+                "Shaders/sample.vert",      //Path to vertex shader
+                "Shaders/sample.frag",      //Path to fragment shader
+                &vecModel,                  //vector container for storage
+                spawnPos,                   //translation
+                0.0f,                       //along x rotation
+                0.0f,                       //along y rotation
+                0.0f,                       //along z rotation
+                5.0f                        //scale
+            );
+
+            glfwSetTime(0.0f);
+        }
+        else if (glfwGetTime() < 3.0f)
+        {
+            std::cout << "NOT YET READY!! CD: " << glfwGetTime() << std::endl;
+        }
+    }
+}
+
 int main(void)
 {
     GLFWwindow* window;
@@ -53,13 +134,8 @@ int main(void)
 
     //Stores all the 3D Models
     std::vector<Model::Model3D*> vecModel;
-    
-    //Offset because we use the camera's position to spawn the object
-    //then we add the  offset to make it spawn in front of the camera
-    glm::vec3 spawnOffset = glm::vec3(0.0f, 0.0f, -10.0f);
 
     //Constant vectors and matrices
-    glm::vec3 worldUp = glm::normalize(glm::vec3(0.0f, 1.0f, 0.0f));    //World's Up
     glm::mat4 projection = glm::perspective(glm::radians(60.0f), (height / width), 0.1f, 100.0f);
     glm::mat4 identity_matrix = glm::mat4(1.0f);             
 
@@ -77,37 +153,45 @@ int main(void)
     gladLoadGL();
 
     glfwSetKeyCallback(window, Key_Callback);
+    glfwSetCursorPosCallback(window, Mouse_Callback);
+     
+    //Timer function set to 0
+    glfwSetTime(3.0f);
+
+    //For Delta Time Function
+    previousTime = glfwGetTime();
 
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //View Matrix inside the while loop to we can move the camera
-        glm::vec3 cameraPos = glm::vec3(x_mod, y_mod, 10.0f);
-        glm::vec3 cameraCenter = glm::vec3(x_mod, y_mod, 0.0f);
-        glm::mat4 view_matrix = glm::lookAt(cameraPos, cameraCenter, worldUp);
+        double currentTime = glfwGetTime();
+        deltaTime = currentTime - previousTime;
+        previousTime = currentTime;
 
+        //View Matrix inside the while loop to we can move the camera
+        glm::mat4 view_matrix = glm::lookAt(cameraPos, cameraPos + cameraCenter, worldUp);
+
+        /* * * * * * Spawn Script * * * * * */
+
+        //If space bar is pressed
+        int state = glfwGetKey(window, GLFW_KEY_SPACE);
+
+        //Spawn Location -> Position in front of camera with an offset
+        glm::vec3 spawnPos = cameraPos + (10.f * cameraCenter);
+        
+        //Spawns the first game object
+        /* BlasterA, Blaster B, and Blaster C are taken from kenny.nl */
         if (vecModel.empty())
         {
-            SpawnObject("3D/blasterA.obj", "Shaders/sample.vert", "Shaders/sample.frag", &vecModel, cameraPos + spawnOffset, 90.0f, -90.0f, 90.0f, 5.0f);
+            SpawnModel(GLFW_PRESS, vecModel, spawnPos);
         }
 
         //Spawn Handler
-        int state = glfwGetKey(window, GLFW_KEY_SPACE);
-        if (state == GLFW_PRESS)
-        {
-            SpawnObject(
-                "3D/blasterA.obj",          //Path to 3D Model
-                "Shaders/sample.vert",      //Path to vertex shader
-                "Shaders/sample.frag",      //Path to fragment shader
-                &vecModel,                  //vector container for storage
-                cameraPos + spawnOffset,    //translation
-                90.0f,                      //along x rotation
-                -90.0f,                     //along y rotation
-                90.0f,                      //along z rotation
-                5.0f                        //scale
-            );
-        }
+        SpawnModel(state, vecModel, spawnPos);
+
+
+        /* * * * * * * * * * * * * * * * * */
 
         //Draw Call, we pass on the view and projection matrices
         for (int i = 0; i < vecModel.size(); i++)
---
+++ main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void SpawnModel(int state, std::vector<Model::Model3D*>& vecModel, glm::vec3& cameraPos, glm::vec3& spawnOffset);
